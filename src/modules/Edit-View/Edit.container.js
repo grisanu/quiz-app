12082e4a9d1e8cbc67c4cde20d16de5587476c5a
Grisanu Naing <mgrisanu@gmail.com>
2016-10-11 19:19:40 -0700

    (refactor) Seperate Edit view in to presentational and container components

--- src/modules/Edit-View/Edit.container.js
+++ src/modules/Edit-View/Edit.container.js
@@ -2,6 +2,17 @@ import { connect } from 'react-redux';
 import Edit from './Edit.view.js';
 import { makeQuestionsList } from './makeQuestionsListActions';
 
+class Edit extends React.Component {
+
+}
+
+Edit.propTypes = {
+  questions: PropTypes.array.isRequired,
+  session: PropTypes.object.isRequired,
+  questionsByQuizId: PropTypes.object.isRequired,
+  makeQuestionsList: PropTypes.func.isRequired
+};
+
 const mapStateToProps = state => ({
   questionsByQuizId: state.questionsByQuizId,
   questions: state.questions,
